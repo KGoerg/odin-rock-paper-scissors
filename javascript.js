100a66ef0d3d7c8f7c15657aca742deac6a1ccc0
kgoerg <224106412+KGoerg@users.noreply.github.com>
2025-09-08 14:23:38 -0500

    Change = to === for randomNumber comparisons
    I didn't realize I needed === as a comparison operator. I had the right idea, but was using = (an assignment operator), which was incorrect.

--- javascript.js
+++ javascript.js
@@ -15,11 +15,11 @@
 
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3);
-    if (randomNumber = 0) {
+    if (randomNumber === 0) {
         return "Rock";
-    } else if (randomNumber = 1) {
+    } else if (randomNumber === 1) {
         return "Paper";
-    } else (randomNumber = 2)
+    } else
         return "Scissors";
     }
 
